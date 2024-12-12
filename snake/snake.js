f5c7f7e0c5237b84691767904ab0384488df1182
lVagtBendoski <157419244+lVagtBendoski@users.noreply.github.com>
2024-12-12 11:24:10 -0500

    Update snake.js

--- snake/snake.js
+++ snake/snake.js
@@ -98,7 +98,7 @@ function moveSnake(){
   //console.log(snake[0].x + "   " +  snake[0].y)
   snake.unshift(head);
 
-  const has_eaten_food = snake[0].x === food_x && snake[0].y === f10_y;
+  const has_eaten_food = snake[0].x === food_x && snake[0].y === f_y;
   if (has_eaten_food) {
     score += 10;
     document.getElementById('score').textContent = score;
